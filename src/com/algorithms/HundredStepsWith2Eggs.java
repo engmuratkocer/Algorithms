864106762a09b47d751adbbb3a6e4426759961cb
Murat KOCER <eng.muratkocer@gmail.com>
2018-02-24 19:23:47 +0100

    Btree Algorithms

--- src/com/algorithms/HundredStepsWith2Eggs.java
+++ src/com/algorithms/HundredStepsWith2Eggs.java
@@ -1,18 +1,23 @@
 package com.algorithms;
 
+/**
+ *
+ * Calculate minumum steps eggs broken, at 100 step stairs, by totally moving eggs minumum.
+ *
+ */
 public class HundredStepsWith2Eggs {
 
     public static void main(String[] args) {
 
         int a[] = new int[101];
 
-        for (int i = 1; i<=100; i++) {
-            a[i] = (100/i)+(i-1);
+        for (int i = 1; i <= 100; i++) {
+            a[i] = (100 / i) + (i - 1);
         }
 
         System.out.println(" constructed array ");
-        for (int i :a
-             ) {
+        for (int i : a
+                ) {
             System.out.println(a[i]);
         }
 
@@ -20,12 +25,12 @@ public class HundredStepsWith2Eggs {
 
         int min = -1;
         for (int i = 1; i <= 101; i++) {
-            if (min == -1){
+            if (min == -1) {
                 min = a[i];
             }
 
-            System.out.println(" compare min : "+min+"with "+ a[i] + " " + i);
-            if (a[i] < min){
+            System.out.println(" compare min : " + min + "with " + a[i] + " " + i);
+            if (a[i] < min) {
                 min = a[i];
             }
         }
